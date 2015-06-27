17f6d801a472443b2216dfad95535f161fa54fca
casavik <casavik@gmail.com>
2015-06-27 15:44:06 -0600

    Creacion del proyecto

---
+++ Warming/Warming/Clase-4/Cuadratica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warming.Clase_4
+{
+    class Cuadratica
+    {
+        double ValorA;
+        double ValorB;
+        double ValorC;
+
+        public Cuadratica() 
+        { 
+        }
+
+        public Cuadratica(double a, double b, double c) 
+        {
+        }
+     }
+}
